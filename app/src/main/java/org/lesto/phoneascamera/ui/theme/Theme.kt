abdcc3c9ca16fb0430543559c04c11b4bb1a7586
lesto <lesto@fante>
2026-03-18 00:56:52 +0100

    remove unused file, added license

--- app/src/main/java/org/lesto/phoneascamera/ui/theme/Theme.kt
+++ app/src/main/java/org/lesto/phoneascamera/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package org.lesto.phoneascamera.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
